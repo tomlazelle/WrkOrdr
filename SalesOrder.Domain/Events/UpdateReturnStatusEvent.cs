089cf5364e0c6ff61210ab111345c4e9fa2f1931
Tom LaZelle <tomlazelle@yahoo.com>
2017-01-03 14:56:35 -0600

    adding missed files

---
+++ SalesOrder.Domain/Events/UpdateReturnStatusEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EventSource.Framework;
+using Sales.Common;
+
+namespace Sales.Domain.Events
+{
+    public class UpdateReturnStatusEvent:VersionedEvent<Guid>
+    {
+        public UpdateReturnStatusEvent(Guid id,ReturnStatus status, string returnId)
+        {
+            SourceId = id;
+            Status = status;
+            ReturnId = returnId;
+        }
+        public ReturnStatus Status { get; }
+        public string ReturnId { get; }
+    }
+}
